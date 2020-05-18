7c6ce3b7186e795521f915656d524886884b882e
Andreas Kraft <kraftandi@gmail.com>
2020-05-18 20:48:25 +0200

    new

--- pages/api/test.js
+++ pages/api/test.js
@@ -1,5 +1,7 @@
 export default (req, res) => {
-    console.log(req, res);
+    console.log(req);
+    console.log(req.files);
+    console.log(res);
     res.send({ resp: "TEST Response" });
   };
   
